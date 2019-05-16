c4c2961c8b08acc2e1cb902337aa669528b1f2ca
tjennyyy <jenny_tang@gmx.net>
2019-05-16 09:23:11 +0200

    a1

--- src/Main_A1.java
+++ src/Main_A1.java
@@ -2,6 +2,15 @@ public class Main_A1 {
 
     public static void main(String[] args) {
 
-        System.out.println("");
+        try{
+            int[] myNumbers = {1,2,3};
+            System.out.println(myNumbers[10]);
+        } catch (Exception e) {
+            System.out.println("catch");
+        }
+        finally {
+            System.out.println("try catch finished");
+        }
     }
+
 }
