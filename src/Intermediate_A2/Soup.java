1ad88f0141e11a4290c47c97d7a8bc8b743fede9
judith <judithmarialang@gmail.com>
2019-05-16 10:55:57 +0200

    adds soup class to project

---
+++ src/Intermediate_A2/Soup.java
@@ -0,0 +1,31 @@
+package Intermediate_A2;
+
+public class Soup {
+
+    private String soupName;
+    private String portionVolume;
+
+    public Soup (String soupName, String portionVolume){
+        this.soupName = soupName;
+        this.portionVolume = portionVolume;
+
+    }
+
+    public String getSoupName(){
+        return soupName;
+    }
+
+    public void setSoupName(String soupName){
+        this.soupName = soupName;
+    }
+
+    public String getPortionVolume(){
+        return portionVolume;
+    }
+
+    public void setPortionVolume(){
+        this.portionVolume = portionVolume;
+    }
+
+
+}
